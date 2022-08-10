78979638de0d26e197ec697c05f25fcad07b75b2
Mars <nuriev.mars@gmail.com>
2022-08-10 21:02:02 +0300

    Задача dz01 решена

---
+++ sem_1/dz01/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+int value1 = 10;
+int value2 = 10;
+if (value1 > value2)
+    {Console.WriteLine("max = " + value1);}
+else if (value2 == value1)
+{
+    Console.WriteLine(value1 + " равно " + value2);
+
+}
+else
+{
+    Console.WriteLine("max = " + value2);
+}
+
