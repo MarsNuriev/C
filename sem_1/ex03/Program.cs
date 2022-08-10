531a06369670dcfc04bee5027e6dc941b2d850d5
Mars <nuriev.mars@gmail.com>
2022-08-10 20:45:16 +0300

    Задача ex03 решена

---
+++ sem_1/ex03/Program.cs
@@ -0,0 +1,18 @@
+﻿// дни недели по номеру
+int value = 0;
+if(value == 1)
+    {Console.WriteLine("Понедельник");}
+else if (value == 2)
+    {Console.WriteLine("Вторник");}    
+else if (value == 3)
+    {Console.WriteLine("Среда");}
+else if (value == 4)
+    {Console.WriteLine("Четверг");}
+else if (value == 5)
+    {Console.WriteLine("Пятница");}
+else if (value == 6)
+    {Console.WriteLine("Суббота");}
+else if (value == 7)
+    {Console.WriteLine("Воскресенье");}
+else
+    {Console.WriteLine("Введите число от 1 до 7");}
