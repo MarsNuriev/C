fea5bf7be6802e4150936a400b16114f69e4b712
Mars <nuriev.mars@gmail.com>
2022-08-18 14:01:05 +0300

    Задача dz02 исправлено на любое количество символов в числе

--- sem_2/dz02/Program.cs
+++ sem_2/dz02/Program.cs
@@ -3,27 +3,34 @@
 //78 -> третьей цифры нет
 //32679 -> 6
 
-int RandomInt (int min, int max)
+internal class Program
 {
-    return new Random().Next(min, max);
-}
+    private static void Main(string[] args)
+    {
+        int RandomInt(int min, int max)
+        {
+            return new Random().Next(min, max);
+        }
 
-void ex01(int val)
-{
-    int a = val % 100;
-    int b = a % 10;
-    Console.WriteLine(val);
-    Console.WriteLine(b);
-}
-//int val = RandomInt(10,1000);
-//Console.WriteLine(val);
-int val1 = RandomInt(10,1000);
-if ( val1 < 100)
-{    
-    Console.WriteLine(val1);
-    Console.WriteLine("третьей цифры нет");
-}
-else
-{
-    ex01(val1);
+        int val = RandomInt(10, 1000);
+        if (val < 100)
+        {
+            Console.WriteLine(val);
+            Console.WriteLine("третьей цифры нет");
+        }
+        else
+        {
+            int i = Convert.ToString(val).Length;
+            int step = i - 3;
+            double del = 1;
+            del = Math.Pow(10, step);
+            int delInt = Convert.ToInt32(del);
+            int val1 = val / delInt;
+            int val2 = val1 % 10;
+
+            Console.WriteLine(val);
+            Console.WriteLine(delInt);
+            Console.WriteLine(val2);
+        }
+    }
 }
