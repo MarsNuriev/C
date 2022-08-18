82907312faf5a7323fedad48eccf23ee806af0a2
Mars <nuriev.mars@gmail.com>
2022-08-18 14:04:26 +0300

    Задача dz01 строки 15 - 19 добавил табы

--- sem_2/dz01/Program.cs
+++ sem_2/dz01/Program.cs
@@ -4,25 +4,31 @@
 // 782 -> 8
 // 918 -> 1
 
-int RandomInt (int min, int max)
+internal class Program
 {
-    return new Random().Next(min, max);
+    private static void Main(string[] args)
+    {
+        int RandomInt(int min, int max)
+        {
+            return new Random().Next(min, max);
+        }
+
+        void ex01(int val)
+        {
+            // int a = val / 100;
+            int b = val % 100;
+            int a = b / 10;
+
+            Console.WriteLine(val);
+            Console.WriteLine(a);
+        }
+
+        //if (a > b)
+        //    {Console.WriteLine(a);}
+        //else
+        //    {Console.WriteLine(b);}
+        //}
+
+        ex01(RandomInt(100, 1000));
+    }
 }
-
-void ex01 (int val)
-{
-// int a = val / 100;
-int b = val % 100;
-int a = b / 10;
-
-Console.WriteLine(val);
-Console.WriteLine(a);
-}
-
-//if (a > b)
-//    {Console.WriteLine(a);}
-//else
-//    {Console.WriteLine(b);}
-//}
-
-ex01(RandomInt(100, 1000));
