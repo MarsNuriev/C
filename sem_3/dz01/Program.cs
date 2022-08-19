486e33f841ff85f73fc8b393d7d1fe7556950c2f
Mars <nuriev.mars@gmail.com>
2022-08-19 11:24:43 +0300

    Задача dz01 исправлена, вызов через функцию

--- sem_3/dz01/Program.cs
+++ sem_3/dz01/Program.cs
@@ -3,35 +3,46 @@
 //12821 -> да
 //23432 -> да
 
-int RandomInt (int min, int max)
+internal class Program
 {
-    return new Random().Next(min, max);
-}
+    private static void Main(string[] args)
+    {
+        int RandomInt(int min, int max)
+        {
+            return new Random().Next(min, max);
+        }
 
-int val = RandomInt(12021, 12023);
+        int val = RandomInt(12021, 12023);
 
 
-Console.WriteLine(val);
-// int[] array = new int[5];
-// for (int i = 4; i >= 0; i--)
-// {   
-//     int array(i) = val % 10;
-//     val = val /10;
-// }
-// Console.WriteLine(array);
-int val4 = val % 10;
-Console.WriteLine(val4);
-int val3 = (val / 10) % 10;
-Console.WriteLine(val3);
-int val1 = (val / 1000) % 10;
-Console.WriteLine(val1);
-int val0 = (val / 10000) % 10;
-Console.WriteLine(val0);
-if (val4 == val0 && val3 == val1)
-{
-    Console.WriteLine("Число палиндром");
+        Console.WriteLine(val);
+        // int[] array = new int[5];
+        // for (int i = 4; i >= 0; i--)
+        // {   
+        //     int array(i) = val % 10;
+        //     val = val /10;
+        // }
+        // Console.WriteLine(array);
+        void palindrom(int val)
+        {
+            int val4 = val % 10;
+            Console.WriteLine(val4);
+            int val3 = val / 10 % 10;
+            Console.WriteLine(val3);
+            int val1 = val / 1000 % 10;
+            Console.WriteLine(val1);
+            int val0 = val / 10000 % 10;
+            Console.WriteLine(val0);
+            if (val4 == val0 && val3 == val1)
+            {
+                Console.WriteLine("Число палиндром");
+            }
+            else
+            {
+                Console.WriteLine("Число не палиндром");
+            }
+        }
+    palindrom(RandomInt(12021, 12023));
+    }
 }
-else
-{
-    Console.WriteLine("Число не палиндром");
-}
+            
