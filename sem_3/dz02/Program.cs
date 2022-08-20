d9718d1c79c13d6e57dca6a9edb177e013cb05f5
Mars <nuriev.mars@gmail.com>
2022-08-20 19:54:59 +0300

    Задача dz02 исправлена, вызов через функцию

--- sem_3/dz02/Program.cs
+++ sem_3/dz02/Program.cs
@@ -2,14 +2,10 @@
 //A (3,6,8); B (2,1,-7), -> 15.84
 //A (7,-5, 0); B (1,-1,9) -> 11.53
 
-int x1 = 5;
-int y1 = 5;
-int z1 = 5;
+double dlina (int x1, int y1, int x2, int y2, int z1, int z2)
+{
+    double dl = Math.Sqrt(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2));
+    return dl;
+}
 
-int x2 = 10;
-int y2 = 10;
-int z2 = 10;
-
-double dlina = Math.Pow(Math.Pow(x1-x2,2) + Math.Pow(y1-y2,2) + Math.Pow(z1-z2,2),0.5);
-
-Console.WriteLine(dlina);
+Console.WriteLine(dlina (15, -5, 25, -5, 35, -5));
