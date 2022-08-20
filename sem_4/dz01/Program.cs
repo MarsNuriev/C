42c360d761d020e15468b3e4ce08c13a863be047
Mars <nuriev.mars@gmail.com>
2022-08-21 00:11:36 +0300

    Задача sem_4 dz01 решена

---
+++ sem_4/dz01/Program.cs
@@ -0,0 +1,18 @@
+﻿// // Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// 3, 5 -> 243 (3⁵)
+
+// 2, 4 -> 16
+
+
+int step (int a, int b)
+{
+    int res = a;
+    for (int i = 1; i < b; i++)
+    {
+        res = a*res;
+    }
+    return res;
+}
+int c = step(2,5);
+Console.WriteLine(c);
