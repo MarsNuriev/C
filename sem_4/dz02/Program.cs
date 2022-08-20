d0b701286b3aab82999d3e72770fc3414031fc79
Mars <nuriev.mars@gmail.com>
2022-08-21 00:25:15 +0300

    Задача sem_4 dz02 решена

---
+++ sem_4/dz02/Program.cs
@@ -0,0 +1,22 @@
+﻿// // Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+
+
+int SumCifr (int a)
+{
+    int sum = 0;
+    int lenghth = Convert.ToString(a).Length;
+    for (int i = 0; i <= lenghth; i++)
+    {
+        sum = sum + a % 10;
+        a = a / 10;
+    }
+    return sum;
+}
+int c = SumCifr(4522);
+Console.WriteLine(c);
