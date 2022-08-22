b1f64e3af5dc5ad07e505579e3c061f032742a81
Mars <nuriev.mars@gmail.com>
2022-08-22 19:44:25 +0300

    Задача sem_4 dz03 исправлена - исправил функцию создания случайного масссива

--- sem_4/dz03/Program.cs
+++ sem_4/dz03/Program.cs
@@ -2,37 +2,41 @@
 
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 
-// 6, 1, 33 -> [6, 1, 33]
-int [] arr = {1,2,3,4};
-void PrintArr(int [] array)
+internal class Program
 {
-    int count = arr.Length;
-    Console.Write("[");
-    for (int i = 0; i < count; i++)
+    private static void Main(string[] args)
     {
-        Console.Write($"{array[i]}, ");
+        // 6, 1, 33 -> [6, 1, 33]
+        int[] arr = { 1, 2, 3, 4 };
+        void PrintArr(int[] array)
+        {
+            int count = array.Length;
+            Console.Write("[");
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+            Console.Write("]");
+        }
+        PrintArr(arr);
+
+        System.Console.WriteLine();
+
+        int[] CreateRandArr(int N)
+        {
+            int [] array = new int[N];
+            //System.Console.WriteLine(array);
+            for (int i = 0; i < N; i++)
+            {
+                array[i] = new Random().Next(100, 10000);
+            }
+
+            return array;
+
+        }
+
+
+        int[] arr3 = CreateRandArr(10);
+        PrintArr(arr3);
     }
-    Console.Write("]");
 }
-PrintArr(arr);
-
-System.Console.WriteLine();
-
-// int [] arr2 = new int [4];
-int [] CreateRandArrSize (int min, int max)
-{
-    int N = new Random().Next(min,max);
-    return new int[N];
-}
-
-void FillPrintRandArr (int [] array)
-{
-    int len = array.Length;
-    for (int i = 0; i < len; i++)
-    {
-        array[i] = new Random().Next(0, 10);
-        System.Console.Write($"{array[i]} ");
-    }
-}
-int [] arr3 = CreateRandArrSize(1,9);
-FillPrintRandArr(arr3);
