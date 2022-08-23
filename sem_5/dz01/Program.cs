f69a0571a957cb32218a2762c46f6ff25e9df531
Mars <nuriev.mars@gmail.com>
2022-08-23 11:19:51 +0300

    Задача sem_5 dz01 исправлена - поправил формат

--- sem_5/dz01/Program.cs
+++ sem_5/dz01/Program.cs
@@ -3,30 +3,30 @@
 // [345, 897, 568, 234] -> 2
 
 int[] CreateRandArr(int N)
-        {
-            int [] array = new int[N];
-            //System.Console.WriteLine(array);
-            for (int i = 0; i < N; i++)
-            {
-                array[i] = new Random().Next(100, 1000);
-            }
+{
+    int[] array = new int[N];
+    //System.Console.WriteLine(array);
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = new Random().Next(100, 1000);
+    }
 
-            return array;
+    return array;
 
-        }
+}
 void PrintArr(int[] array)
-        {
-            int count = array.Length;
-            Console.Write("[");
-            for (int i = 0; i < count; i++)
-            {
-                Console.Write($"{array[i]}, ");
-            }
-            Console.Write("]");
-            System.Console.WriteLine();
-        }
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+    System.Console.WriteLine();
+}
 
-int Chet (int [] array)
+int Chet(int[] array)
 {
     int count1 = 0;
     int len = array.Length;
@@ -41,7 +41,7 @@ int Chet (int [] array)
 }
 
 
-int [] arr1 = CreateRandArr(10);
+int[] arr1 = CreateRandArr(10);
 PrintArr(arr1);
 int a = Chet(arr1);
 System.Console.WriteLine(a);
