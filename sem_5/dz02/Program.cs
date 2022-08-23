1ee57a3d7a617dc2460c4c41695cb38d9ab22e18
Mars <nuriev.mars@gmail.com>
2022-08-23 11:20:25 +0300

    Задача sem_5 dz02 исправлена - поправил формат

--- sem_5/dz02/Program.cs
+++ sem_5/dz02/Program.cs
@@ -4,29 +4,29 @@
 
 // [-4, -6, 89, 6] -> 0
 int[] CreateRandArr(int N)
-        {
-            int [] array = new int[N];
-            //System.Console.WriteLine(array);
-            for (int i = 0; i < N; i++)
-            {
-                array[i] = new Random().Next(1, 10);
-            }
+{
+    int[] array = new int[N];
+    //System.Console.WriteLine(array);
+    for (int i = 0; i < N; i++)
+    {
+        array[i] = new Random().Next(1, 10);
+    }
 
-            return array;
-        }
+    return array;
+}
 
 void PrintArr(int[] array)
-        {
-            int count = array.Length;
-            Console.Write("[");
-            for (int i = 0; i < count; i++)
-            {
-                Console.Write($"{array[i]}, ");
-            }
-            Console.Write("]");
-            System.Console.WriteLine();
-        }
-int SumNechEl (int [] array)
+{
+    int count = array.Length;
+    Console.Write("[");
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]}, ");
+    }
+    Console.Write("]");
+    System.Console.WriteLine();
+}
+int SumNechEl(int[] array)
 {
     int sum = 0;
     int count1 = array.Length;
@@ -40,7 +40,7 @@ int SumNechEl (int [] array)
     return sum;
 }
 
-int [] arr = CreateRandArr(10);
+int[] arr = CreateRandArr(10);
 PrintArr(arr);
 int a = SumNechEl(arr);
 System.Console.WriteLine(a);
