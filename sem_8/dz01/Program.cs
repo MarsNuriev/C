fb3abf143c100867f17804105d41961f174f7eb3
Mars <nuriev.mars@gmail.com>
2022-08-30 16:12:00 +0300

    Задача sem_8 dz01 решена

---
+++ sem_8/dz01/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+int[,] CreateArray(int m, int n)
+{
+    int[,] array = new int[m, n];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+    return array;
+}
+
+void PrintArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+void SortArr(int[,] array)
+{
+    int tmp = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            for (int j = 0; j < array.GetLength(1) - 1; j++)
+            {
+                if (array[i, j] < array[i, j + 1])
+                {
+                    tmp = array[i, j];
+                    array[i, j] = array[i, j + 1];
+                    array[i, j + 1] = tmp;
+                }
+            }
+        }
+    }
+}
+    int[,] arr = CreateArray(7, 7);
+    PrintArray(arr);
+    SortArr(arr);
+    PrintArray(arr);
