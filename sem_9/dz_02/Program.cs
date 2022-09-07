477d404ae9640de908dbe0f33a91d9d8899112fc
Mars <nuriev.mars@gmail.com>
2022-09-07 09:12:53 +0300

    Задача sem_9 dz_02 решена

---
+++ sem_9/dz_02/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+int SummNatur(int m, int n)
+{
+    int sum = m;
+
+    if (n > m)
+    {
+        sum = sum + SummNatur(m + 1, n);
+    }
+    return sum;
+
+}
+
+int summa = SummNatur(1, 15);
+System.Console.WriteLine(summa);
