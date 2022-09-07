edd95dd0cdd1c785bdb5340cc4e9cae94317f59a
Mars <nuriev.mars@gmail.com>
2022-09-07 14:47:46 +0300

    задача sem_9 dz_03 решена

--- sem_9/dz_03/Program.cs
+++ sem_9/dz_03/Program.cs
@@ -2,3 +2,13 @@
 
 // m = 2, n = 3 -> A(m,n) = 9
 
+long A(long m, long n)
+{
+    if (m == 0) return n + 1;
+    if (m > 0 && n == 0) return A(m - 1, 1);
+    if (m > 0 && n > 0) return A(m - 1, A(m, n - 1));
+    return A(m, n);
+}
+
+long Fi = A(3,0);
+System.Console.WriteLine(Fi);
